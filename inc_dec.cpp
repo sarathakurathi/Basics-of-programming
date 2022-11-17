fe4237d481e302e92f07baa2c333fe63ef92e829
sarathakurathi <sarathakurathi@gmail.com>
2022-11-17 12:56:50 +0530

    Add files via upload

--- inc_dec.cpp
+++ inc_dec.cpp
@@ -1,34 +1,51 @@
 #include <Arduino.h>
-//Declaring all variables as integers
-int Z=0,Y=1,X=0,W=0;
-int D,C,B,A;
+
+int Z,Y,X,W;      //Declaring all variables as integers
+int A,B,C,D;
 
 //Code released under GNU GPL.  Free to use for anything.
 void disp_7447(int D, int C, int B, int A)
 {
   digitalWrite(2, A); //LSB
-  digitalWrite(3, B); 
-  digitalWrite(4, C); 
+  digitalWrite(3, B);
+  digitalWrite(4, C);
   digitalWrite(5, D); //MSB
 
 }
+
 // the setup function runs once when you press reset or power the board
 void setup() {
-    pinMode(2, OUTPUT);  
+  
+    pinMode(13, OUTPUT);
+    pinMode(2, OUTPUT);
     pinMode(3, OUTPUT);
     pinMode(4, OUTPUT);
     pinMode(5, OUTPUT);
+    pinMode(6, INPUT);
+    pinMode(7, INPUT);
+    pinMode(8, INPUT);
+    pinMode(9, INPUT);
+
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  A=(!W&&!Z) || (!W&&!X&&!Y) 
-  B=(W&&!X&&!Y&&!Z) || (!W&&X&&!Y&&!Z) || (W&&!X&&Y&&!Z) || (!W&&X&&Y&&!Z);
-  C=(W&&X&&!Y&&!Z) || (!W&&!X&&Y&&!Z) || (W&&!X&&Y&&!Z) || (!W&&X&&Y&&!Z);
-  D = (W&&X&&Y&&!Z)||(!W&&!X&&!Y&&Z);
-  
-disp_7447(D,C,B,A);  
+
+digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level) 
+delay(750);             // wait for a second
+
+W = digitalRead(6);//LSB
+X = digitalRead(7);
+Y = digitalRead(8);
+Z = digitalRead(9);//MSB
+
+A=(!W&&!X&&!Y&&!Z) || (!W&&!X&&!Y&&Z) || (!W&&X&&Y&&!Z) || (!W&&!X&&Y&&!Z) || (!W&&X&&!Y&&!Z);
+B=(!W&&!X&&!Y&&Z) || (W&&X&&Y&&!Z) || (!W&&!X&&Y&&!Z) || (W&&X&&!Y&&!Z);
+C=(!W&&!X&&!Y&&Z) || (W&&X&&Y&&!Z) || (!W&&X&&Y&&!Z) || (W&&!X&&Y&&!Z);
+D = (!W&&!X&&!Y&&!Z)||(W&&!X&&!Y&&Z);
+
+digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+delay(500);             // wait for a second
+
+disp_7447(D,C,B,A);
 }
-//&& is the AND operation
-// || is the OR operation
-// ! is the NOT operation
